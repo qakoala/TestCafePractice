9388021ac95ef3a602eb116c40e18acaa6892132
qazakaria <96100804+qazakaria@users.noreply.github.com>
2022-01-26 22:02:46 -0800

    Add LiveMode
    Updated TestCafe to add LiveMode in script

--- tests/Mytests.js
+++ tests/Mytests.js
@@ -1,14 +1,25 @@
 import { Selector } from 'testcafe';
 
-fixture`Getting Started`
+fixture`My Test Plan`
     .page`http://devexpress.github.io/testcafe/example`;
 
 
-    test('My first test', async t => {
+    test.skip('My first test', async t => {
         await t
-        .typeText('[#developer-name]', 'John Smith')
+        .typeText('#developer-name', 'John Smith')
         .click('#submit-button')
 
         // Use the assertion to check if the actual header text is equal to the expected one
-        .expect(Selector('#article-header').innerText).eql('Thank you, John Smith!333');
+        .expect(Selector('#article-header').innerText).eql('Thank you, John Smith!');
+    });
+
+    test('My 2nd Test', async t => {
+        await t
+
+        .navigateTo('http://www.google.com')
+        .typeText('[title="Search"]', 'John Smith')
+        .click('[value="Google Search"]')
+
+        // Use the assertion to check if the actual header text is equal to the expected one
+        .expect(Selector('[aria-label="Search"]').value).eql('John Smith');
     });

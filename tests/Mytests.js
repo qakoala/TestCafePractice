45f6079cdd7c436c7aa1d32813a32627cd366b7c
qazakaria <96100804+qazakaria@users.noreply.github.com>
2022-01-30 21:29:07 -0800

    Added new test for Automation Site
    Practice TestCafe with another test

--- tests/Mytests.js
+++ tests/Mytests.js
@@ -13,7 +13,7 @@ fixture`My Test Plan`
         .expect(Selector('#article-header').innerText).eql('Thank you, John Smith!');
     });
 
-    test('My 2nd Test', async t => {
+    test.skip('My 2nd Test', async t => {
         await t
 
         .navigateTo('http://www.google.com')
@@ -22,4 +22,21 @@ fixture`My Test Plan`
 
         // Use the assertion to check if the actual header text is equal to the expected one
         .expect(Selector('[aria-label="Search"]').value).eql('John Smith');
+    });
+
+    test('Automation Test Site', async t => {
+        await t
+
+        // Visit Automation Test Site and Login
+        .navigateTo('https://saucedemo.com')
+        .typeText('[id="user-name"]', 'standard_user')
+        .typeText('[id="password"]', 'secret_sauce')
+        .click('[id="login-button"]')
+
+        // Add Backpack product then go to Cart
+        .click('[id="add-to-cart-sauce-labs-backpack"]')
+        .click('.shopping_cart_link')
+
+        // Verify Product Appears in Cart
+        .expect(Selector('.inventory_item_name').innerText).eql('Sauce Labs Backpack');
     });
